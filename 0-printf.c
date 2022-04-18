a15cc09d7522f4da7231f4b07f2a9f055b2a3891
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-18 00:51:31 -0700

    0-main.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,56 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
 /**
  * ptintf - Produces output.
  * @format: A character string.
  * Return: Return the number of characters printed.
  */
-
 int _printf(const char *format, ...)
 {
 	va_list ap;
 
 	indexf = 0;
 	indexd = 0;
-
 	char *dest;
 
 	dest = malloc(sizeof(char) * (indexd + 1));
 	if (dest == 0)
-	{
 		return (NULL);
-	}
 
 	va_start(ap, format);
 	for (; format[indexf] != '\0'; f++)
 	{
 		if (format[indexf] == '%')
-		{
-			f++;
+		f++;
+
 		if (format[indexf] == 'c')
 		{
 			dest[indexd] = (char)va_arg(ap, int);
-
 			indexd++;
 		}
-
 		if (format[indexf] == 's')
 		{
 			dest[indexd] = (char)va_arg(ap, int);
-
 			indexd++;
 		}
-
 		else
 		{
 			return (NULL);
 		}
-
 		return (dest);
-		}
-
-		dest == '\0';
 	}
 }
