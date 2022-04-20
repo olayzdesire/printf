708c9484cad683fa3c741ed66711f88ae59d2dcf
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-20 15:59:20 -0700

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -11,14 +11,12 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 
-	int indexf = 0;
+	int indexf = 0, indexs = 0;
 	int count = 0;
+	char *str;
 	char *save;
 
 	save = malloc(sizeof(char) * 1024);
-
-	if (save == NULL)
-		return (1);
 
 	while (format[indexf] != '\0')
 	{
@@ -28,7 +26,7 @@ int _printf(const char *format, ...)
 			indexf++;
 			if (format[indexf] == 'c')
 			{
-				va_arg(ap, int);
+				save[count] = va_arg(ap, int);
 				count += putchar(format[indexf]);
 				count++;
 			}
@@ -39,8 +37,15 @@ int _printf(const char *format, ...)
 				indexf++;
 				if (format[indexf] == 's')
 				{
-					va_arg(ap, char*);
+					str = va_arg(ap, char*);
+					indexs = 0;
+					while (str[indexs])
+					{
+						save[count] = str[indexs];
+						count++;
+						indexs++;
 					count += putchar(format[indexf]);
+					}
 				}
 			}
 		}
@@ -52,5 +57,6 @@ int _printf(const char *format, ...)
 		indexf++;
 	}
 	va_end(ap);
+	free(save);
 	return (indexf);
 }
