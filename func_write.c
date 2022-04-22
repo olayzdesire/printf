c12996cd54774e1adbbffb81409a1bc7d9a0cc62
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-22 04:53:47 -0700

    func_write.c

---
+++ func_write.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * write - writes c to stdout.
+ * @c: char to be written to stdout.
+ * return: 0 on success, -1 on error.
+ */
+
+int _write(char c)
+{
+	write(1, &c, 1);
+}
